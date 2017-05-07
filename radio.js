0b7d4dbbabe515af2fa6d726d3bbe6247dc2318e
Kevin Kearney <kevkearney@gmail.com>
2017-05-07 13:06:45 +0000

    Instituted refactor changes

--- radio.js
+++ radio.js
@@ -1,9 +1,50 @@
 var sizeof = require('object-sizeof');
 var weatherDB = require("./data/weatherDB");
-var Parser = require('binary-parser').Parser;
-var weatherMsg = new Parser().endianess('big').uint16('Lux').int16('BaroTemperature').int16('BaroPressure').int16('Humidity').int16('Temperature');
-var sleepTime = 30;
+var cppMsg = require('../node_modules/cppmsg/cppMsg.js');
+var reverse = require("buffer-reverse");
+
+var weatherdatamsg = new cppMsg.msg(
+	[
+		['MessageType', 'int8'],
+		['Temperature', 'int16'],
+		['Humidty', 'int16'],
+		['BaroPressure', 'int16'],
+		['BaroTemperature', 'int16'],
+		['Lux', 'uint16']
+	]
+);
+
+var lightningMsg = new cppMsg.msg(
+	[
+		['MessageType', 'int8'],
+		['EventType', 'int16'],
+		['Distance', 'int16'],
+		['Intensity', 'int16']
+	]
+);
+
+var weathercontrolmsg = new cppMsg.msg(
+	[
+		['SleepTime', 'int16'],
+		['LightningIndoors', 'bool'],
+		['LightningTune', 'int16'],
+		['LightningNoiseFloor', 'int16'],
+		['RadioPower', 'int16'],
+		['SystemReset', 'bool']
+	]
+);
+
 var socket = require('socket.io-client')('http://localhost:3000');
+
+
+//Test code, remove when complete
+var response = {
+	sleeptime: 10,
+	lightningIndoors: true,
+	lightningTune: 4,
+	lightningNoiseFloor: 4,
+	radioPower: 3
+};
 
 var NRF24 = require('nrf'),
 	spiDev = "/dev/spidev0.0",
@@ -11,64 +52,48 @@ var NRF24 = require('nrf'),
 	irqPin = 25, //var ce = require("./gpio").connect(cePin)
 	pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2];
 var nrf = NRF24.connect(spiDev, cePin, irqPin);
-//nrf._debug = true;git 
+//nrf._debug = true; 
 nrf.channel(0x4c); // Set channel to 76
 nrf.transmitPower('PA_MAX');
 nrf.dataRate('1Mbps') // Set data rate to 1Mbps
 nrf.crcBytes(2) // Set the CRC to 2
 nrf.autoRetransmit({
 	count: 500,
-	delay: 0
+	delay: 15
 });
+
 nrf.begin(function() {
 	console.log("Radio recevier listening.");
 	var rx = nrf.openPipe('rx', pipes[1]),
 		tx = nrf.openPipe('tx', pipes[0]);
 
-	rx.on('data', function(d) {		
-		var text = reverse(d.toString('utf8'));
+	rx.on('data', function(d) {
+		var typeCode = reverse(d).readUIntBE(0, 1);;
+		console.log(typeCode);
 		
-		var response = '{sleepTime:' + sleepTime + '}';
-			tx.write(reverse(response), sizeof(response));
-		console.log("Before Check!")
-		if (text.charAt(0) == 'L') {
-			var lightningData = new Object();
-			
-			console.log("LIGHTNING EVENT DETECTED!")
-			var trimmedText = text.substr(1);
-			console.log(trimmedText);		
-		
-			var n = trimmedText.indexOf(":");
-			if (n == -1) {
-				lightningData.eventType = trimmedText;
-				
-				weatherDB.insertLightning(lightningData, () => {
-				console.log("complete!")
-				});			
-			} else {
-				lightningData.eventType = trimmedText.substring(0,n);
-				lightningData.distance = trimmedText.substring(13);
-				weatherDB.insertLightning(lightningData, () => {
-				console.log("complete!") 
-				});
-			}
-		} else {
-			var weatherData = weatherMsg.parse(d);
-			weatherDB.insertWeather(weatherData,function(currentDate){
-				weatherData.SampleDate = currentDate;
-                                socket.emit('weather message', weatherData);
-			});
+		if (typeCode === 1) {
+			var data = weatherdatamsg.decodeMsg(reverse(d));
+			console.log(data);
+
 		}
-		
+		else if(typeCode === 2) {
+			var otherdata = lightningMsg.decodeMsg(reverse(d));
+			console.log(otherdata);
+		}
+		else{
+				console.warn("No suitable struct found for decode.");
+		}
+		var responseBuf = weathercontrolmsg.encodeMsg(response);
+
+		tx.write(reverse(responseBuf), sizeof(responseBuf));
+
+	});
+	tx.on('error', function(e) {
+		console.warn("Error sending reply.", e);
+		process.exit();
 	});
 	tx.on('error', function(e) {
 		console.warn("Error sending reply.", e);
 		process.exit();
 	});
 });
-
-function reverse(s) {
-	return s.split("").reverse().join("");
-}
-
-
